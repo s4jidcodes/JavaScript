823ef4a935921679613b7c45ec390553cc2e72f0
s4jidcodes <sajid.shah98677@gmail.com>
2026-06-21 20:40:33 +0530

    Initial commit

---
+++ chap3-Javascript with HTML & CSS/index.js
@@ -0,0 +1,8 @@
+console.log('I am coming from index.js')
+
+//comment--> Single line comment
+/* same as css comment for (multi-line comment)*/
+//This method is used to clear the console
+
+console.clear()
+
